e7749830897eeb8e5f305d928dd00b73f62ba215
Sarah Piot <sfg.piot@outlook.com>
2019-10-02 15:22:01 -0600

    Added functions (empty) to TipCounter.cs

---
+++ Assets/Scripts/TipScripts/TipCounter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipCounter : MonoBehaviour
+{
+
+    private totalTips = 0;
+    private comboMultiplier;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Multiplies incoming tips by multipier
+    void ScoreMultiplier()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    // When a level/game ends
+    void ResetScore()
+    {
+    
+    }
+}
