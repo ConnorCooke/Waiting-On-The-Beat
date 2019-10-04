359b2d04bc361ff7ab0223dab064f157d0e2e0fe
Martin Thingvold <mthingvold@gmail.com>
2019-10-04 10:14:07 -0600

    added a bar that scrolls over a circle.
    This will eventually be in tune with the beat.

---
+++ Assets/Scripts/SongScripts/BeatVis.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatVis : MonoBehaviour
+{
+    //This will be determined by bpm
+    public float speed;
+    SpriteRenderer sprite;
+    
+    private bool trigger= false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector2(transform.position.x - speed, transform.position.y);
+        sprite.color = new Color(sprite.color.r,sprite.color.g,sprite.color.b,sprite.color.a+0.02f);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (trigger)
+        {
+            Destroy(gameObject);
+        }
+        trigger = true;
+        sprite.color = new Color(sprite.color.r + 25, sprite.color.g - 25, sprite.color.b + 10);
+    }
+}
