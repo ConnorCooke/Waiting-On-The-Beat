61cb851f6ec3b6a29006d926a59acde709d84610
Martin Thingvold <mthingvold@gmail.com>
2019-10-04 10:24:31 -0600

    Created a Spawner to spawn beat objects

---
+++ Assets/Scripts/SongScripts/BeatSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatSpawner : MonoBehaviour
+{
+    public float spawnRate = 1.0f;
+    public GameObject beatBar;
+    private float nextTimeToSpawn = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Time.time >= nextTimeToSpawn)
+        {
+            Instantiate(beatBar, new Vector2(transform.position.x+5,transform.position.y), Quaternion.identity);
+            nextTimeToSpawn = Time.time + 1f / spawnRate;
+        }
+    }
+}
