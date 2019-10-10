cf92ec72825fa2dd1a8b224c191c116e9d992966
eileenvanheerde <eileen.vanheerde@usask.ca>
2019-10-09 23:09:30 -0600

    added python script to generate list of beats with a sample .wav file (for realsies this time)

---
+++ SpikePrototypes/Essentia_BeatTracker/beatGenerator.py
@@ -0,0 +1,18 @@
+import essentia
+from essentia.standard import *
+import numpy as np
+
+fname = './beat-test.wav'
+audiobeat = MonoLoader(filename=fname)()
+
+rhythm_extractor = RhythmExtractor2013(method="multifeature")
+bpm, beats, b_conf, _, _ = rhythm_extractor(audiobeat)
+
+print("BPM: ", bpm)
+print("Beat Positions (sec.): ", beats)
+print("Beat Estimation Confidence: ", b_conf)
+
+f = open("./beat-test.txt", "w+")
+f.write(str(beats).replace('[',' ').replace(']',''))
+f.close()
+
