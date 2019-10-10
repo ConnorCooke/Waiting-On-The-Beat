a3173651e0f5b20eb8f9d1a52a633a8b0da4a878
eileenvanheerde <eileen.vanheerde@usask.ca>
2019-10-09 23:16:36 -0600

    commented out print statements (uncomment them to print bpm, beat list, & confidence)

--- SpikePrototypes/Essentia_BeatTracker/beatGenerator.py
+++ SpikePrototypes/Essentia_BeatTracker/beatGenerator.py
@@ -8,9 +8,10 @@ audiobeat = MonoLoader(filename=fname)()
 rhythm_extractor = RhythmExtractor2013(method="multifeature")
 bpm, beats, b_conf, _, _ = rhythm_extractor(audiobeat)
 
-print("BPM: ", bpm)
-print("Beat Positions (sec.): ", beats)
-print("Beat Estimation Confidence: ", b_conf)
+# remove uncomment to print bpm, list of beats, & b_conf to console
+# print("BPM: ", bpm)
+# print("Beat Positions (sec.): ", beats)
+# print("Beat Estimation Confidence: ", b_conf)
 
 f = open("./beat-test.txt", "w+")
 f.write(str(beats).replace('[',' ').replace(']',''))
