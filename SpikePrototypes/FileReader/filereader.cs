4975de3f4527a420b99c2a84c454ad074039ce60
van Heerde <evv446@usask.ca>
2019-10-18 13:59:38 -0600

    made c# script to read in txt file in Unity

--- SpikePrototypes/FileReader/filereader.cs
+++ SpikePrototypes/FileReader/filereader.cs
@@ -3,4 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
+public class filereader : MonoBehaviour
+{
+    // Reads in text file
+    [RuntimeInitializeOnLoadMethod]
+    static void LoadJson()
+    {
+        List<float> beats = new List<float>();
+        StreamReader r = new StreamReader("./Assets/beat-test.txt");
+
+        while(!r.EndOfStream)
+        {
+            string ln = r.ReadLine();
+            beats.Add(float.Parse(ln));
+        }
+
+        r.Close();
+
+        // Debug.Log(beats[beats.Count-1]);
+    }
+}
 
