d5219a4b125e1f7c1bf8a1ff297ba963fc15ff90
eileenvanheerde <eileen.vanheerde@usask.ca>
2019-10-29 12:04:49 -0600

    Removed some funtions and added/implemented new ones. Need to clarify some things with the group

--- Assets/Scripts/TipScripts/TipCounter.cs
+++ Assets/Scripts/TipScripts/TipCounter.cs
@@ -4,31 +4,40 @@ using UnityEngine;
 
 public class TipCounter : MonoBehaviour
 {
-
-    private int totalTips = 0;
-    private int comboMultiplier;
+    private float totalTips = 0;
+    private float tipMultiplier = 0;
     
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    
-    // Multiplies incoming tips by multipier
-    void ScoreMultiplier()
+
+    // Adds customer tip to totalTips base on tipMultiplier
+    void addTip(float tipValue)
     {
-        
+        float tipToAdd = 0;
+
+        // TODO: discuss with group how tipMultiplier should work
+        if (tipMultiplier != 0)
+        {
+            tipToAdd = tipValue * tipMultiplier;
+        } else
+        {
+            tipToAdd = tipValue;
+        }
+
+        totalTips += tipToAdd;  
+    }
+
+    void updateTipMultiplier(float newMultiplier)
+    {
+        tipMultiplier = newMultiplier;
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-    
-    // When a level/game ends
-    void ResetScore()
-    {
-    
     }
 }
