0a50378089972d0abb1b98fb428c295edf4de865
Josh Stanzeleit <jes857@usask.ca>
2019-10-30 02:07:45 -0600

    recreated FoodOrder and Food entirely, no errors present, but can seem to properly instantiate food objects, still confused about FoodAvailable(Transform) method, can receive orders ffrom the player and send food to the player

--- Assets/Scripts/DataTypes/FoodOrder.cs
+++ Assets/Scripts/DataTypes/FoodOrder.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public abstract class FoodOrder : MonoBehaviour
 {
- 
+    
     public string FoodName { get; set; }
     
     public int CookTimer { get; set; }
+
+    public Food FoodObject;
+
+    public void Spawn(int spot)
+    {
+        Instantiate<Food>(FoodObject, new Vector3(1.46f, (5.49f - spot), 6.58f), Quaternion.identity);
+    }
 
     
 }
