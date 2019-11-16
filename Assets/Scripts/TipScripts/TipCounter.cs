1e8e65ab2fbce305f8228145cfcbe906c39107b3
eileenvanheerde <eileen.vanheerde@usask.ca>
2019-11-15 19:40:11 -0600

    Added PlayerData class & implemented TipCounter

--- Assets/Scripts/TipScripts/TipCounter.cs
+++ Assets/Scripts/TipScripts/TipCounter.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class TipCounter : MonoBehaviour
 {
-    private float totalTips = 0;
-    private float tipMultiplier = 0;
-    
+    private float tipMultiplier;
+    private float currTotalTips;
+    public ObjectManager objectManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tipMultiplier = 0f;
+        currTotalTips = 0;
     }
 
-    // Adds customer tip to totalTips base on tipMultiplier
+    /**
+     * Adds customer tip to totalTips based on tipMultiplier
+     */
     void addTip(float tipValue)
     {
-        float tipToAdd = 0;
+        float tipToAdd = 0f;
 
         // TODO: discuss with group how tipMultiplier should work
-        if (tipMultiplier != 0)
+        if (tipMultiplier != 0f)
         {
             tipToAdd = tipValue * tipMultiplier;
         } else
@@ -27,12 +31,25 @@ public class TipCounter : MonoBehaviour
             tipToAdd = tipValue;
         }
 
-        totalTips += tipToAdd;  
+        currTotalTips += tipToAdd;
     }
 
+    /**
+     * Updates the tipMultiplier value
+     */
     void updateTipMultiplier(float newMultiplier)
     {
         tipMultiplier = newMultiplier;
+    }
+
+    /**
+     * 
+     */
+     void receiveScoreRequest()
+    {
+        objectManager.GiveTipTotal(currTotalTips);
+        // add tips earned from lvl to PlayerData.totalTips value
+        PlayerData.setTotalTips(PlayerData.getTotalTips() + currTotalTips);
     }
 
     // Update is called once per frame
