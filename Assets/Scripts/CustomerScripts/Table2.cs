6a6a74509bd3516664f821a6c3acef4e0fb8ae3a
Martin Thingvold <mthingvold@gmail.com>
2019-12-05 17:41:13 -0600

    add table and kitchen script

---
+++ Assets/Scripts/CustomerScripts/Table2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MartinTable : Table
+{
+    new protected GameObject[] customers = new GameObject[6];
+    new protected GameObject[] customersFood = new GameObject[6];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
