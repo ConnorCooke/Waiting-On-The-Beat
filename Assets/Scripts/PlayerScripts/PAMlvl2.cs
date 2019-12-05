9d18703c9487b9ce403dfa50d19446e51dd72e7f
Josh Stanzeleit <jes857@mail.usask.ca>
2019-12-05 17:38:25 -0600

    changed PAMlvl2 and objman2, created them'

---
+++ Assets/Scripts/PlayerScripts/PAMlvl2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PAMlvl2 : PlayerActionAndMovement
+{
+    // Start is called before the first frame update
+	protected int width = 14;
+	protected int height = 10;
+	
+	
+	protected override void Start(){
+		
+		playerPosition = new int[]{3,2};
+		tileContents = new int[,]{{0,0,0,3,3,4,4,4,4,3,3,0,0,0},{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,1,1,1,1,0,0,0,0,0},
+		{0,0,0,0,0,0,1,1,0,0,0,0,0,0},
+		{0,0,0,0,0,1,1,1,1,0,0,0,0,0},
+		{0,0,0,0,0,0,1,1,0,0,0,0,0,0},
+		{0,0,0,0,0,1,1,1,1,0,0,0,0,0},
+		{0,0,0,0,0,0,1,1,0,0,0,0,0,0},
+		{0,0,0,0,0,1,1,1,1,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+
+	}
+	
+
+
+   
+}
