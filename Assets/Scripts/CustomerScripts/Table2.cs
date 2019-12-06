81a49158852e6bf9e8fb14085cc9415578898693
Josh Stanzeleit <jes857@mail.usask.ca>
2019-12-05 20:52:17 -0600

    added spawning customers, it works. they spawn in a perfect order instead of random order

--- Assets/Scripts/CustomerScripts/Table2.cs
+++ Assets/Scripts/CustomerScripts/Table2.cs
@@ -2,8 +2,38 @@
 
 public class Table2 : Table
 {
-    new protected GameObject[] customers = new GameObject[6];
-    new protected GameObject[] customersFood = new GameObject[6];
+
+	protected override void Start()
+    {
+        drinkSprites = Resources.LoadAll<Sprite>("Sprites/SetDressing/drinksnoanimation");
+		customers = new GameObject[6];
+		customersFood = new GameObject[6];
+    }
+
+	protected override void Update()
+    {
+        if (!requestedCustomer)
+        {
+            int index = 0;
+            while(index < 6)
+            {
+                if(customers[index] is null)
+                {
+                    RequestCustomer();
+                    index = 6;
+                }
+                index++;
+            }
+        }
+    }
+
+	protected override void CustomerReceived(int index)
+    {
+        Vector3 pos = customers[index].transform.position;
+        
+        objectManager.CustomerReadyToOrder(new Vector3(pos.x,pos.y,pos.z));
+        
+    }
 
     // Start is called before the first frame update
     protected override void SetCustomerTransform(int idx, GameObject customer)
@@ -19,7 +49,7 @@ public class Table2 : Table
         }
         if (idx == 0)
         {
-            SetTransform(-2, (float)1);
+            SetTransform(-2, (float)2);
             customer.GetComponent<CustomerSpriteManager>().faceEast(upperBaseLayer);
         }
         else if (idx == 1)
@@ -29,12 +59,12 @@ public class Table2 : Table
         }
         else if (idx == 2)
         {
-            SetTransform(-2, (float)-1);
+            SetTransform(-2, (float)-2);
             customer.GetComponent<CustomerSpriteManager>().faceEast(lowerBaseLayer);
         }
         else if (idx == 3)
         {
-            SetTransform(1, (float)1);
+            SetTransform(1, (float)2);
             customer.GetComponent<CustomerSpriteManager>().faceEast(lowerBaseLayer);
         }
         else if (idx == 4)
@@ -44,7 +74,7 @@ public class Table2 : Table
         }
         else
         {
-            SetTransform(1, (float)-1);
+            SetTransform(1, (float)-2);
             customer.GetComponent<CustomerSpriteManager>().faceWest(lowerBaseLayer);
         }
     }
