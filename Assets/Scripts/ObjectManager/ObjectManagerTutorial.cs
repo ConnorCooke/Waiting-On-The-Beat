647d92eb0640236767dc1905bf8644236a7dd52d
Connor <cookeconnor98@gmail.com>
2019-12-06 12:12:10 -0600

    initial tutorial level

---
+++ Assets/Scripts/ObjectManager/ObjectManagerTutorial.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManagerTutorial : ObjectManager
+{
+    public UIManagerTutorial uiManagerTutorial;
+
+    public override void CustomerPaid(float tip, Vector3 position)
+    {
+        base.CustomerPaid(tip, position);
+        uiManagerTutorial.NextTutorialSection();
+    }
+
+    public override void CustomerReadyToOrder(Vector3 position)
+    {
+        base.CustomerReadyToOrder(position);
+        uiManagerTutorial.NextTutorialSection();
+    }
+}
---
+++ Assets/Scripts/UIScripts/UIManagerTutorial.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManagerTutorial : UIManager
+{
+    protected int index = 0;
+    GameObject[] tutorialObjects;
+    public int[] numberOfTextDuringPause;
+    bool paused = false;
+    public TypeItOut typeItOut;
+    protected int currentLimits = 0;
+
+    protected override void Start()
+    {
+        base.Start();
+        tutorialObjects = GameObject.FindGameObjectsWithTag("TutorialPause");
+        hideTutorialPaused();
+    }
+
+    public void NextTutorialSection()
+    {
+        if(currentLimits < numberOfTextDuringPause.Length)
+        {
+            if (paused)
+            {
+                typeItOut.WriteOutTutorial();
+                index++;
+                if (index == numberOfTextDuringPause[currentLimits])
+                {
+                    paused = false;
+                    index = 0;
+                    currentLimits++;
+                }
+            }
+            else
+            {
+                tutorialPauseControl();
+                paused = true;
+            }
+        }
+        else
+        {
+
+        }
+    }
+    //controls the pausing of the scene
+    public void tutorialPauseControl()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0f;
+            showTutorialPaused();
+        }
+        else if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1f;
+            hideTutorialPaused();
+        }
+    }
+
+    //shows objects with ShowOnPause tag
+    public void showTutorialPaused()
+    {
+        foreach (GameObject g in tutorialObjects)
+        {
+            g.SetActive(true);
+        }
+        nextLevelButton.SetActive(false);
+        AudioListener.pause = true;
+    }
+
+    //hides objects with ShowOnPause tag
+    public void hideTutorialPaused()
+    {
+        foreach (GameObject g in tutorialObjects)
+        {
+            g.SetActive(false);
+        }
+        AudioListener.pause = false;
+    }
+}
